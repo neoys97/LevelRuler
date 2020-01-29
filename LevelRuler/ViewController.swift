8621e8112ce1cbb9a329658492718983ef82d7dd
neoys97 <neoys97@gmail.com>
2020-01-29 12:30:27 +0800

    Initial Commit

---
+++ LevelRuler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LevelRuler
+//
+//  Created by Neo Yi Siang on 29/1/2020.
+//  Copyright © 2020 Neo Yi Siang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
